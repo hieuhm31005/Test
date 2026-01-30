c7a0c5a6a9e07cf423e35736f2e7d797752a7f1e
hieuhm <minhhieu31005@gmail.com>
2026-01-30 08:52:32 -0800

    dc

--- index.cpp
+++ index.cpp
@@ -5,5 +5,16 @@ int main()
 {
   cout << "Hello, World" << endl;
   cout << "Hello Hieu" << endl;
+  cout << "Hello Linux" << endl;
+  int n; cin >> n;
+  int *a = new int[n];
+  for(int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+  }
+  for(auto it : a)
+  {
+    cout << it << " ";
+  }
   return 0;
 }
