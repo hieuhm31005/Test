6d804fc90561ab35f75e8b9a3930709861a0e6b5
hieuhm <minhhieu31005@gmail.com>
2026-01-30 09:05:46 -0800

    commit on the another device using corresponding ssh key

--- index.cpp
+++ index.cpp
@@ -1,5 +1,21 @@
-@include<iostream>
+#include<iostream>
+#include<algorithm>
+
 using namespace std;
+
+void bubble_sort(int *a, int n)
+{
+  for(int i = 0; i < n - 1; i++)
+  {
+    for(int j = 0 ; j < n - i - 1; j++)
+    {
+      if(a[j] > a[j + 1])
+      {
+        swap(a[j], a[j + 1]);
+      }
+    }
+  }
+}
 
 int main()
 {
@@ -12,9 +28,17 @@ int main()
   {
     cin >> a[i];
   }
-  for(auto it : a)
+  cout << "This is the array before sorting" << endl;
+  for(int i = 0; i < n; i++)
   {
-    cout << it << " ";
+    cout << a[i] << " ";
+  }
+  
+  cout << "\nThis is the array after using sort Algo" << endl;
+  bubble_sort(a, n);
+  for(int i = 0; i < n; i++)
+  {
+    cout << a[i] << " ";
   }
   return 0;
 }
